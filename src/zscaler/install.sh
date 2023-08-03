5353a4fc332e367f5b4ac94912b7204a41ea80fe
Pablo Torres <pablo.torres.t@gmail.com>
2023-08-03 18:13:33 +0000

    remove old variables

--- src/zscaler/install.sh
+++ src/zscaler/install.sh
@@ -4,8 +4,7 @@ PYTHON_BIN_PATHS="${PYTHONBINPATHS}"
 
 set -e
 
-echo "Activating feature 'color'"
-echo "The provided favorite color is: ${FAVORITE}"
+echo "Activating feature Zscaler"
 
 
 # The 'install.sh' entrypoint script is always executed as the root user.
@@ -14,11 +13,6 @@ echo "The provided favorite color is: ${FAVORITE}"
 # These may be useful in instances where the context of the final
 # remoteUser or containerUser is useful.
 # For more details, see https://containers.dev/implementors/features#user-env-var
-echo "The effective dev container remoteUser is '$_REMOTE_USER'"
-echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
-
-echo "The effective dev container containerUser is '$_CONTAINER_USER'"
-echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
 
 
 copy_certificate() {
